bf0769ff0de6ed082b0c424741cee9b75ecc949a
Matthew Knowles <matthewyknowles@gmail.com>
2017-08-15 16:34:55 -0400

    structure for fizz buzz

---
+++ spec/fizzbuzz.spec.js
@@ -0,0 +1,5 @@
+describe("Fizz Buzz", () => {
+    it("should return string of 1 for number 1", () => {
+        expect(fizzBuzzTranslator(1)).toEqual("1");
+    })
+})
---
+++ src/fizzbuzz.js
@@ -0,0 +1,3 @@
+function fizzBuzzTranslator(toTranslate) {
+    return "";
+}
